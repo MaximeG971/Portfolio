34b1aaa8a3b1d16a7a5ecb5661d02ed1a194223e
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-12-15 22:04:05 +0100

    suppression du CV pour en faire une rubrique à part

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,34 +1,26 @@
+import "./Contact.css";
+
 function Contact() {
   return (
-    <div>
+    <div className="contact">
       <h1>Contact</h1>
-      <div className="contact-mail">
-        <h3>Mail</h3>
-        <a href="mailto:maximejordan.gael@gmail.com">
-          <button type="button" className="mail">
-            <img src="/logo-mail.png" width={30} height={30} />
-            maximejordan.gael@gmail.com
-          </button>
-        </a>
-      </div>
-      <div className="contact-reseau">
-        <h3>Réseaux Pro</h3>
+      <div className="liens-contact">
         <a href="https://linkedin.com/in/maxime-gael">
-          <button type="button" className="linkedin">
+          <button type="button" className="contact-linkedin">
             <img src="/logo-linkedin.png" width={30} height={30} />
             Linkedin
           </button>
         </a>
         <a href="https://github.com/MaximeG971">
-          <button type="button" className="github">
+          <button type="button" className="contact-github">
             <img src="/logo-github.png" width={30} height={30} />
             Github
           </button>
         </a>
-        <h3>Mon CV</h3>
-        <a href="/CV-Dev-Alternance.pdf" download>
-          <button type="button" className="CV">
-            Télécharger le CV
+        <a href="mailto:maximejordan.gael@gmail.com">
+          <button type="button" className="contact-mail">
+            <img src="/logo-mail.png" width={30} height={30} />
+            maximejordan.gael@gmail.com
           </button>
         </a>
       </div>

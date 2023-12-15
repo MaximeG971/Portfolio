ae798b0196aa24d5a818b08e6e3af7c9aef25650
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-12-15 21:13:59 +0100

    création du composant pour la navbar

---
+++ src/components/navbar/Nav.jsx
@@ -0,0 +1,13 @@
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>Skills</li>
+        <li>Projets</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;

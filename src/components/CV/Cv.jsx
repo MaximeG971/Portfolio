26b9db64523ef627c9a974905e6c32af17b6bf19
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-01-04 09:51:01 +0100

    modification de l'image d'aperçu du cv et du cv téléchargé

--- src/components/CV/Cv.jsx
+++ src/components/CV/Cv.jsx
@@ -6,9 +6,14 @@ function Cv() {
       <h1>Mon CV</h1>
       <div className="mon-cv">
         <div className="img-cv">
-          <img src="/cv.png" alt="aperçu de mon cv" width={300} height={450} />
+          <img
+            src="/cv-dev-img.png"
+            alt="aperçu de mon cv"
+            width={300}
+            height={450}
+          />
         </div>
-        <a href="/CV-Dev-Alternance.pdf" download>
+        <a href="/CV-Dev.pdf" download>
           <button type="button" className="CV">
             Télécharger le CV
           </button>
